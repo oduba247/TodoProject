b33fbdcb1ffa64cf8e0961dab096d104a888d792
tommy <odubatomiwa@gmail.com>
2022-12-08 14:55:36 +0100

    first commit

---
+++ todoApp/views.py
@@ -0,0 +1,63 @@
+from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from .forms import TodoForm
+from .models import Todo
+# Create your views here.
+def todo_list(request):
+    todo =Todo.objects.all()
+    print(todo)
+
+    context ={
+        "todo_list" : todo
+    }
+    return render(request, 'todoApp/todo_list.html',context)
+
+# def todo_list(request):
+#     return render(request, 'todo_list2.html')
+def todo_detail(request,id):
+    todo = Todo.objects.get(id=id)
+    context = {
+        "todo" : todo
+    }
+    return render(request,"todoApp/todo_detail.html",context)
+
+def todo_create(request):
+    form = TodoForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+        # # print(form.cleaned_data)
+        # name =form.cleaned_data["name"]
+        # due_date = form.cleaned_data["due_date"]
+        # print(name,due_date)
+
+        # new_todo =Todo.objects.create(name=name,due_date=due_date)
+        # pass
+    context ={"form":form}
+    return render(request,"todoApp/todo_create.html",context)
+
+def todo_update(request, id):
+    todo = Todo.objects.get(id=id)
+    form = TodoForm(request.POST or None, instance=todo)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+        # # print(form.cleaned_data)
+        # name =form.cleaned_data["name"]
+        # due_date = form.cleaned_data["due_date"]
+        # print(name,due_date)
+
+        # new_todo =Todo.objects.create(name=name,due_date=due_date)
+        # pass
+    context ={"form":form}
+    return render(request,"todoApp/todo_update.html",context)
+
+def todo_delete(request,id):
+        todo = Todo.objects.get(id=id)
+        todo.delete()
+
+        return render('/')
+        
+
+
+    
